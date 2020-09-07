466e27eff39091fc4833a792fb6b5cc84f39289b
gaoning <mjyuyu@qq.com>
2020-09-07 15:10:30 +0800

    init

---
+++ src/Router/Navigation.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View} from 'react-native';
+
+interface Props {}
+
+const Navigation = (props: Props) => {
+  console.log('props', props);
+  return (
+    <View>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam eum
+      perspiciatis magnam minima quisquam impedit quae reiciendis minus
+      doloremque dicta cumque vero voluptatum excepturi, quaerat, repellat modi
+      aut earum porro!
+    </View>
+  );
+};
+
+export default Navigation;
